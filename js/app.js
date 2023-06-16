8383fdbc84c4a6848b06ee08509e33cd9667bf5f
Chris Palmer <christopherpalmer283@gmail.com>
2023-06-16 14:20:51 +0100

    problem 3 solved

--- js/app.js
+++ js/app.js
@@ -31,11 +31,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-  const multiply = [
+  const multi = [
     a * b,
     "The product of " + a + " and " + b + " is " + a * b + ".",
   ];
-  return multiply;
+  return multi;
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -56,11 +56,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  let sum1 = sum(a, b)[0];
+
+  let sum2 = sum(sum1, c)[0];
+
+  let multi1 = multiply(a, b)[0];
+
+  let multi2 = multiply(multi1, c)[0];
+
+  let string1 =
+    "The product of " + a + " and " + b + " and " + c + " is " + multi2 + ".";
+
+  let string2 = a + " and " + b + " and " + c + " sum to " + sum2 + ".";
+
+  return [sum2, multi2, string2, string1];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
